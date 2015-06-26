31fe7c284300776dc16db0574f2fad8b931dbf41
Shu Fujita <osorubeki.fujita@gmail.com>
2015-06-26 09:49:44 +0900

    20150626_095140 Bundle Update

---
+++ hanzomon_20150625.rb
@@ -0,0 +1,33 @@
+require 'httpclient'
+require 'json'
+
+Encoding.default_external = "utf-8"
+
+ACCESS_TOKEN = '9790afccf7295b37c2e20ab98092579a9dc7057389d54a2865305e7096b1b0cf'
+
+puts Time.now.to_s
+
+response = HTTPClient.new.get( "https://api.tokyometroapp.jp/api/v2/datapoints" , { "rdf:type" => "odpt:TrainInformation" , "odpt:railway" => "odpt.Railway:TokyoMetro.Hanzomon" , "acl:consumerKey"=> ACCESS_TOKEN } )
+puts JSON.parse(response.body).to_s
+
+response = HTTPClient.new.get( "https://api.tokyometroapp.jp/api/v2/datapoints" , { "rdf:type" => "odpt:Train" , "odpt:railway" => "odpt.Railway:TokyoMetro.Hanzomon" , "acl:consumerKey"=> ACCESS_TOKEN } )
+puts JSON.parse(response.body).to_s
+
+__END__
+
+2015-06-25 08:17:56 +0900
+[
+  {
+    "@context"=>"http://vocab.tokyometroapp.jp/context_odpt_TrainInformation.json",
+    "@id"=>"urn:ucode:_00001C000000000000010000030C3BE5",
+    "dc:date"=>"2015-06-25T08:15:02+09:00",
+    "dct:valid"=>"2015-06-25T08:20:02+09:00",
+    "odpt:operator"=>"odpt.Operator:TokyoMetro",
+    "odpt:railway"=>"odpt.Railway:TokyoMetro.Hanzomon",
+    "odpt:timeOfOrigin"=>"2015-06-25T07:14:00+09:00",
+    "odpt:trainInformationStatus"=>"ダイヤ乱れ",
+    "odpt:trainInformationText"=>"6時35分頃、東急田園都市線 たまプラーザ駅で人身事故のため、ダイヤが乱れています。この影響で女性専用車両を中止しています。只今、東京メトロ線、都営地下鉄線、ＪＲ線、東武線に振替輸送を実施しています。詳しくは、駅係員にお尋ねください。",
+    "@type"=>"odpt:TrainInformation"
+  }
+]
+[]
