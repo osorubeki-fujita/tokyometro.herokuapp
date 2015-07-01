716dd83807a0481281d0a67d82831fb3f8174902
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-01 11:31:24 +0900

    20150701_113555 Update - Rake task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   include TwitterProcessor
 
   def index
     set_twitter_processor( :tokyo_metro )
+    @station_infos = ::Station::Info.all.where( operator_id: ::ApplicationHelper.tokyo_metro.id )
     render 'index' , layout: 'application'
   end
 
