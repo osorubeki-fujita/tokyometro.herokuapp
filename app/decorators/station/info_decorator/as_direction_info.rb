56b1ba7da82a9642c216a166ba479be60dea4631
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-23 23:12:09 +0900

    20150723_231207 Refactoring - Railway::DirectionDecorator

--- app/decorators/station/info_decorator/as_direction_info.rb
+++ app/decorators/station/info_decorator/as_direction_info.rb
@@ -8,6 +8,14 @@ class Station::InfoDecorator::AsDirectionInfo < TokyoMetro::Factory::Decorate::A
     "for #{ object.name_en }"
   end
 
+  def render_name_ja( with_subname: false )
+    decorator.render_name_ja( with_subname: with_subname , suffix: "方面" )
+  end
+
+  def render_name_en( with_subname: false )
+    decorator.render_name_en( with_subname: with_subname , prefix: "for " )
+  end
+
   def render_simply
     h.render inline: <<-HAML , type: :haml , locals: h_this
 %div{ class: :text_ja }<
@@ -27,13 +35,14 @@ class Station::InfoDecorator::AsDirectionInfo < TokyoMetro::Factory::Decorate::A
     HAML
   end
 
-  def render_precisely
-    h.render inline: <<-HAML , type: :haml , locals: h_decorator
+  def render_precisely( with_subname: false )
+    h_locals_i = h_this.merge({ with_subname: with_subname })
+    h.render inline: <<-HAML , type: :haml , locals: h_locals_i
 %div{ class: :railway_direction }
   %p{ class: :text_ja }<
-    = d.render_name_ja( with_subname: false , suffix: "方面" )
+    = this.render_name_ja( with_subname: false )
   %p{ class: :text_en }<
-    = d.render_name_en( with_subname: false , prefix: "for " )
+    = this.render_name_en( with_subname: false )
     HAML
   end
 
