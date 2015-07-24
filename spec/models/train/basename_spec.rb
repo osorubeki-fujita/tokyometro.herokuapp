56653d3d274308bf315ec99fa33b2effd9e42889
Shu Fujita <osorubeki.fujita@gmail.com>
2015-07-24 21:55:42 +0900

    20150724_215534 Bug fix - RSpec file

--- spec/models/train/basename_spec.rb
+++ spec/models/train/basename_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Train::BaseName, type: :model do
+RSpec.describe Train::Basename, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
