6871b219bebb15f319713b0e38796448e420fe75
Shu Fujita <osorubeki.fujita@gmail.com>
2015-08-03 02:22:56 +0900

    20150803_022245 Bug fix - Operator::AsTrainOwner / Update - list of models and tables

--- app/models/operator/as_train_owner.rb
+++ app/models/operator/as_train_owner.rb
@@ -1,7 +1,7 @@
 class Operator::AsTrainOwner < ActiveRecord::Base
 
   belongs_to :info , class: ::Operator::Info
-  has_one :code_info , class: ::Operator::CodeInfo , through: :info
+  has_one :additional_info , class: ::Operator::AdditionalInfo , through: :info
 
   has_many :train_location_infos , class: ::Train::Location::Info , foreign_key: :operator_as_train_owner_id
   has_many :train_timetable_infos , class: ::Train::Timetable::Info , foreign_key: :operator_as_train_owner_id
